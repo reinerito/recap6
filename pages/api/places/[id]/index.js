6fd66d8dd5eaa45741d0a0b0c3d0f0eb32bca23e
Angela Capillo <hello@angelcapillo.com>
2025-04-25 11:10:01 +0200

    Add get requests for index page and details page in api folder

--- pages/api/places/[id]/index.js
+++ pages/api/places/[id]/index.js
@@ -1,14 +1,23 @@
-import { places } from "../../../../lib/db.js";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
+export default async function handler(request, response) {
+  await dbConnect();
+
   const { id } = request.query;
 
-  const place = places.find((place) => place.id === id);
+  //const place = places.find((place) => place.id === id);
 
-  if (!place) {
-    response.status(404).json({ status: "Not found" });
-    return;
+  if (request.method === "GET") {
+    const place = await Place.findById(id);
+
+    if (!place) {
+      response.status(404).json({ status: "Not found" });
+      return;
+    }
+
+    response.status(200).json(place);
   }
 
-  response.status(200).json(place);
+  response.status(405).json({ status: "Method not allowed." });
 }
--- pages/api/places/index.js
+++ pages/api/places/index.js
@@ -1,6 +1,15 @@
-import { places } from "../../../lib/db";
+import dbConnect from "@/db/connect";
+import Place from "@/db/models/Place";
 
-export default function handler(request, response) {
-  response.status(200).json(places);
-  return;
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const places = await Place.find();
+
+    response.status(200).json(places);
+    return;
+  }
+
+  response.status(405).json({ status: "Method not allowed." });
 }
